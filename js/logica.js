7b24f4ad1df8b0b68f2013c4eb8ee21eb32e8c82
Ben Smith <bsmith@scnctech.com>
2025-02-24 15:39:24 -0800

    fixes en videos lentos

--- js/logica.js
+++ js/logica.js
@@ -1,22 +1,26 @@
 async function buscarListaDeEquipos() {
-    const response = await fetch('https://esbendev.com/timbersgoaldust/datos/listaDeEquipos.json');
+    const response = await fetch('../datos/listaDeEquipos.json');
+    // const response = await fetch('https://esbendev.com/timbersgoaldust/datos/listaDeEquipos.json');
     const data = await response.json();
     return data;
 }
 
 async function buscarListaDeGoles() {
-    const response = await fetch('https://esbendev.com/timbersgoaldust/datos/listaDeGoles.json');
+    const response = await fetch('../datos/listaDeGoles.json');
+    // const response = await fetch('https://esbendev.com/timbersgoaldust/datos/listaDeGoles.json');
     const data = await response.json();
     return data;
 }
 
 async function buscarListaDeJugadores() {
-    const response = await fetch('https://esbendev.com/timbersgoaldust/datos/listaDeJugadores.json');
+    const response = await fetch('../datos/listaDeJugadores.json');
+    // const response = await fetch('https://esbendev.com/timbersgoaldust/datos/listaDeJugadores.json');
     const data = await response.json();
     return data;
 }
 
 function getTeamLogo(equipos,equipoBuscado) {
     console.log(equipos[equipoBuscado]);
-    return `https://esbendev.com/timbersgoaldust/imagenes/logos/${equipos[equipoBuscado]}`;
+    return `../imagenes/logos/${equipos[equipoBuscado]}`;
+    // return `https://esbendev.com/timbersgoaldust/imagenes/logos/${equipos[equipoBuscado]}`;
 }
